52954cf6f42ad445ccd5c618e808099dc2ec1eee
Sebastião Felix <54052660+Raposa-Dev@users.noreply.github.com>
2024-08-11 11:05:59 -0300

    First commit

---
+++ src/route.rb
@@ -0,0 +1,17 @@
+require 'webrick'
+
+module AppRoutes
+  def self.call(request, response)
+    response.status = 200
+    response.content_type = 'text/html'
+    response.body = File.read('../index.html')
+  end
+end
+
+module AboutRoutes
+  def self.call(request, response)
+    response.status = 200
+    response.content_type =   'text/html'
+    response.body = File.read('../about.html')
+  end
+end  
---
+++ src/server.rb
@@ -0,0 +1,21 @@
+name = "Server CLOSE!"
+
+puts name
+
+require 'webrick'
+require './route'
+
+server = WEBrick::HTTPServer.new(Port: 3000)
+
+trap 'INT' do server.shutdown end
+
+server.mount_proc '/index.html' do |request, response|
+  AppRoutes.call(request, response)
+end
+
+server.mount_proc '/about.html' do |request, response|
+  AboutRoutes.call(request, response)
+end
+
+server.start
+
